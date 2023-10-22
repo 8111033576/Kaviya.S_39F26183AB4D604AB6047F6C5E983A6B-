ff6a8c452291097c7cc0c053052874af44885d2f
8111033576 <144327180+8111033576@users.noreply.github.com>
2023-10-22 21:51:54 +0530

    Add files via upload

---
+++ Challange 3.1.py
@@ -0,0 +1,12 @@
+def LinearSearchProduct(productList, targetProduct):
+   indices = []
+   for index, product in enumerate(productList):
+     
+        if product == targetProduct:
+            indices.append(index)
+   return indices
+
+product = ["shoes", "boots",  "loafer", "shoes", "sandal", "shoes"]
+target = "shoes"
+result = LinearSearchProduct(product, target)
+print(result)
